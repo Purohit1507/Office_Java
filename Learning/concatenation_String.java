41eedb7058f560efb611fb5cc70fa6fc8db01a45
PARTH <PUROHITPARTH1@GMAIL.COM>
2023-03-23 06:46:08 +0530

    Create concatenation_String.java

---
+++ Learning/concatenation_String.java
@@ -0,0 +1,50 @@
+package Learning;
+
+public class concatenation_String {
+
+	public static void main(String[] args) {
+		
+		//Concatenation
+	    String firstname="Tony";
+		String Lastname="stark";
+		String fullname=firstname + "@" + Lastname;
+		System.out.println(fullname);
+		
+		System.out.println(fullname.length());
+		
+		
+	     //charAct
+//		for (int i=0; i<fullname.length(); i++)
+//		{
+//			System.out.println(fullname.charAt(i));
+//			
+			
+			
+			
+			//Compare
+			String name1="tony";
+			String name2="tony1";
+			
+			if(name1.compareTo(name2)==0)
+			{
+			     System.out.println("Strings are equal");
+			}
+			else
+			{
+				System.out.println("Strings are not equal");
+			}
+			/*
+			* 1) String1>String 2
+			* 2) String1=String 2
+			* 3) String1<String3
+			* 
+			*
+			*
+			*
+			*/
+			
+			
+		}
+	}
+	
+
